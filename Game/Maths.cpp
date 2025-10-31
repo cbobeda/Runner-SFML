143f05546e357fe8e696b8394d75c55c35493c66
cbobeda <cbobeda@gaming.tech>
2025-10-31 10:32:16 +0100

    fix instrinction opcity on death

--- Game/Maths.cpp
+++ Game/Maths.cpp
@@ -125,6 +125,7 @@ int main()
             playing = false;
             window.clear();
             instruction.setString("Press escape to leave or middle click to restart");
+            instruction.setFillColor(sf::Color(255, 255, 255, 255));
             instruction.setPosition(sf::Vector2f(window.getSize().x / 2 - instruction.getLocalBounds().size.x / 2, window.getSize().y / 2 - instruction.getLocalBounds().size.y / 2 + 100));
             window.draw(instruction);
             window.draw(death);
